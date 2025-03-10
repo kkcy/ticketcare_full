e157b4e3bb66b401a3cb491dc3026027e451d232
kkcy <rickykkcy@gmail.com>
2025-03-10 17:02:51 +0800

    chore: remove LiveBlocks & collaboration

--- apps/app/app/(authenticated)/page.tsx
+++ apps/app/app/(authenticated)/page.tsx
@@ -4,18 +4,10 @@ import { database } from '@repo/database';
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
-import { AvatarStack } from './components/avatar-stack';
-import { Cursors } from './components/cursors';
 import { Header } from './components/header';
 
 const title = 'Acme Inc';
 const description = 'My application.';
-
-const CollaborationProvider = dynamic(() =>
-  import('./components/collaboration-provider').then(
-    (mod) => mod.CollaborationProvider
-  )
-);
 
 export const metadata: Metadata = {
   title,
@@ -32,14 +24,7 @@ const App = async () => {
 
   return (
     <>
-      <Header pages={['Building Your Application']} page="Data Fetching">
-        {env.LIVEBLOCKS_SECRET && (
-          <CollaborationProvider orgId={orgId}>
-            <AvatarStack />
-            <Cursors />
-          </CollaborationProvider>
-        )}
-      </Header>
+      <Header pages={['Building Your Application']} page="Data Fetching" />
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           {pages.map((page) => (
