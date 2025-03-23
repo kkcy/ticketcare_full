d7bcc3df49554f9fd5f0ec6bacbc6fd2c54bcd72
kkcy <rickykkcy@gmail.com>
2025-03-23 23:29:46 +0800

    chore(ui): add shadow parameter to card

--- packages/design-system/components/ui/card.tsx
+++ packages/design-system/components/ui/card.tsx
@@ -2,12 +2,13 @@ import * as React from "react"
 
 import { cn } from "@repo/design-system/lib/utils"
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
+function Card({ className, shadow = true, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6",
+        {"shadow-sm": shadow},
         className
       )}
       {...props}
@@ -19,7 +20,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-header"
-      className={cn("flex flex-col gap-1.5 px-6", className)}
+      className={cn("flex flex-col gap-1.5 px-4", className)}
       {...props}
     />
   )
@@ -49,7 +50,7 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-6", className)}
+      className={cn("px-4", className)}
       {...props}
     />
   )
@@ -59,7 +60,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-6", className)}
+      className={cn("flex items-center px-4", className)}
       {...props}
     />
   )
